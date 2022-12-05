32a935fb3be186f375a26f3006edfb01337aed77
kindtek <98300737+kindtek@users.noreply.github.com>
2022-12-04 20:19:35 -0800

    docker image naming

--- sdk/docker-solana/build.sh
+++ sdk/docker-solana/build.sh
@@ -30,8 +30,8 @@ cp -f ../../fetch-spl.sh usr/bin/
   ./fetch-spl.sh
 )
 
-docker build -t kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG" .
-docker tag kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG" kindtek/solana-safedb-debian:latest
+docker build -t kindtek/solana-safedb-debian:kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG"-A.1-bullseye .
+docker tag kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG"-A.1-bullseye kindtek/solana-safedb-debian:latest
 
 maybeEcho=
 if [[ -z $CI ]]; then
@@ -45,4 +45,4 @@ else
     fi
   )
 fi
-$maybeEcho docker push kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG"
+$maybeEcho docker push kindtek/solana-safedb-debian:"$CHANNEL_OR_TAG"-A.1-bullseye
