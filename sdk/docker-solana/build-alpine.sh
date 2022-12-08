d9a0cf54a9d8be0e80ac7abebae6111ad9160e7a
kindtek <n8@kindtek.io>
2022-12-08 15:12:03 -0800

    cleaner build.sh

--- sdk/docker-solana/build-alpine.sh
+++ sdk/docker-solana/build-alpine.sh
@@ -34,18 +34,4 @@ docker build -f Dockerfile.alpine -t ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb
 docker tag ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:$SDB_SOL_DOCKER_TAG "${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:latest"
 docker tag $SDB_SOL_DOCKER_IMG:$SDB_SOL_DOCKER_TAG kindtek/solana-alpine:latest
 
-# maybeEcho=
-# if [[ -z $CI ]]; then
-#   echo "Not CI, skipping |docker push|"
-#   maybeEcho="echo"
-# else
-#   (
-#     set +x
-#     if [[ -n $DOCKER_PASSWORD && -n $DOCKER_USERNAME ]]; then
-#       echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-#     fi
-#   )
-# fi
-# $maybeEcho docker push ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:${SDB_SOL_DOCKER_TAG:-latest_debug}
-
-$maybeEcho docker push ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:${SDB_SOL_DOCKER_TAG:-latest_debug}
+docker push ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:${SDB_SOL_DOCKER_TAG:-latest_debug}
--- sdk/docker-solana/build.sh
+++ sdk/docker-solana/build.sh
@@ -30,19 +30,8 @@ cp -f ../../fetch-spl.sh usr/bin/
   ./fetch-spl.sh
 )
 
-docker build -t $SDB_SOL_DOCKER_IMG:$SDB_SOL_DOCKER_TAG .
-docker tag $SDB_SOL_DOCKER_IMG:$SDB_SOL_DOCKER_TAG "${SDB_SOL_DOCKER_IMG}:latest"
+docker build -f Dockerfile.alpine -t ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:${SDB_SOL_DOCKER_TAG:-latest_debug} .
+docker tag ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:$SDB_SOL_DOCKER_TAG "${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:latest"
+docker tag $SDB_SOL_DOCKER_IMG:$SDB_SOL_DOCKER_TAG kindtek/solana-alpine:latest
 
-maybeEcho=
-if [[ -z $CI ]]; then
-  echo "Not CI, skipping |docker push|"
-  maybeEcho="echo"
-else
-  (
-    set +x
-    if [[ -n $DOCKER_PASSWORD && -n $DOCKER_USERNAME ]]; then
-      echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-    fi
-  )
-fi
-$maybeEcho docker push $$SDB_SOL_DOCKER_IMG:$SDB_SOL_DOCKER_TAG
+docker push ${SDB_SOL_DOCKER_IMG:-kindtek/solana-safedb-alpine_debug}:${SDB_SOL_DOCKER_TAG:-latest_debug}
